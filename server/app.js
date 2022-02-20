0e3079632040cbe58317124643b2a7c434813fdd
ArnabReigns <tysonchatterjee@gmail.com>
2022-02-20 13:36:57 +0530

    frontend starting

--- server/app.js
+++ server/app.js
@@ -1,30 +1,17 @@
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
-
+const dotenv = require('dotenv');
 dotenv.config({path:'./config.env'});
-const DB = process.env.DATABASE
 
-mongoose.connect(DB).then(()=>{console.log("Mongo Database Connected")}).catch((err)=>console.log('database not connected'));
+//connect to database
+require('./db/conn');
+const User = require('./model/userSchema');
 
-//middleware
-const middleware = (req,res,next) =>
-{
-  console.log(`about page`)
-  next()
-}
+app.use(express.json());
+app.use(require('./router/auth'));
 
-//main
-app.get("/", (req, res) => {
-  res.send(`Hello user`);
-});
-app.get("/about",middleware, (req, res) => {
-  res.send(`About Page`);
-});
-app.get("/contact", (req, res) => {
-  res.send(`Contant Page`);
-});
 
-const port = 3000;
-app.listen(port, () => `Server running on port ${port}`)
+//listening to port
+const port = process.env.PORT;
+
+app.listen(port, () =>{console.log(" "); console.log('>> SERVER RUNNING ');console.log(`>> PORT : ${port} `)});
